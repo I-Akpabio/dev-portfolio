ac4eaa944d10efd08fc80a0c18066c64953b3cc2
Inyeneobong Akpabio <inyene.akpabio@checklos.com>
2023-10-15 18:22:58 +0100

    updates

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
-  navbar: 'profile/navbar.json',
-  routes: 'profile/routes.json',
-  home: 'profile/home.json',
-  social: 'profile/social.json',
-  about: 'profile/about.json',
-  skills: 'profile/skills.json',
-  education: 'profile/education.json',
-  experiences: 'profile/experiences.json',
-  projects: 'profile/projects.json',
+  navbar: 'https://i-akpabio.github.io/dev-portfolio/profile/navbar.json',
+  routes: 'https://i-akpabio.github.io/dev-portfolio/profile/routes.json',
+  home: 'https://i-akpabio.github.io/dev-portfolio/profile/home.json',
+  social: 'https://i-akpabio.github.io/dev-portfolio/profile/social.json',
+  about: 'https://i-akpabio.github.io/dev-portfolio/profile/about.json',
+  skills: 'https://i-akpabio.github.io/dev-portfolio/profile/skills.json',
+  education: 'https://i-akpabio.github.io/dev-portfolio/profile/education.json',
+  experiences: 'https://i-akpabio.github.io/dev-portfolio/profile/experiences.json',
+  projects: 'https://i-akpabio.github.io/dev-portfolio/profile/projects.json',
 };
 
 export default endpoints;
